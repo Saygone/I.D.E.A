f9da209ee7ad161819ce18dec511a1afaec9afc5
Saygone <fordestiny1902@gmail.com>
2021-11-02 15:01:53 +0300

    Исправлена ошибка с окончаниями

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
 import First, Second, Third
+c=len(Second.Second_w)
+
+if Second.Second_w[c-1] == 'а' and 'я':
+    First.First_w = First.First_w[:-2]
+    First.First_w = First.First_w + 'ая'
 
 print(First.First_w, Second.Second_w, Third.Third_w)
