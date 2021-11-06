fccb9bdcb888dd91374265abdace96aa7efcf876
Saygone <fordestiny1902@gmail.com>
2021-11-06 17:54:29 +0300

    Добавлен задний фон

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
-import First, Second, Third
-c=len(Second.Second_w)
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+import os
 
-if Second.Second_w[c-1] == 'а' and 'я':
+import First
+import Second
+
+c = len(Second.Second_w)
+
+if Second.Second_w[c - 1] == 'а' and 'я':
     First.First_w = First.First_w[:-2]
     First.First_w = First.First_w + 'ая'
 
-print(First.First_w, Second.Second_w, Third.Third_w)
+root = Tk()
+root.title("I.D.E.A")
+root.geometry("400x500")
+
+canvas = tkinter.Canvas(root, height=500, width=400)
+image = Image.open("Wallpaper.jpg")
+photo = ImageTk.PhotoImage(image)
+image = canvas.create_image(0, 0, anchor='nw', image=photo)
+canvas.place(x=201, y=250, anchor=CENTER)
+
+root.mainloop()
